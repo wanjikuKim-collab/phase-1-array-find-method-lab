bacdfde716fc263a3b91f83f09a4eb9020a016ce
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-11-24 22:41:13 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+// Data on Denver Broncos football team performance on the Superbowl
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(array){
+    for(let item of array){
+        if(item.result === 'W'){
+            return item.year
+        }
+    }
+  }
+
+  record.find(superbowlWin)
